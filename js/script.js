7e20be3c1092acff4f1b56b8d1a4d7819030d5bb
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-12-08 11:11:17 +0000

    Added diophantineEquation function at script.js

--- js/script.js
+++ js/script.js
@@ -15,3 +15,43 @@ function getNumberInfo() {
     // Display Information in the Browser
     document.getElementById("numinfo").innerHTML = txt;
 }
+
+/*
+    Function to find the value of D ≤ n in minimal solutions of x
+    for which the largest value of x is obtained.
+    diophantineEquation(7)    returns 5
+    diophantineEquation(100)  returns 61
+    diophantineEquation(409)  returns 409
+    diophantineEquation(500)  returns 421
+    diophantineEquation(1000) returns 661
+*/
+function diophantineEquation(n) {
+    function isSolution(D, numerator, denominator) {
+        return numerator * numerator - BigInt(D) * denominator * denominator === 1n;
+    }
+
+    let result = 0;
+    let biggestX = 0;
+    for (let D=2;D<=n;D++) {
+        let boundary = Math.floor(Math.sqrt(D));
+        if (boundary ** 2 === D) continue;
+        let m = 0n;
+        let d = 1n;
+        let a = BigInt(boundary);
+        let [numerator, prevNumerator] = [a,1n];
+        let [denominator, prevDenominator] = [1n,0n];
+        while (!isSolution(D,numerator,denominator)) {
+            m = d * a - m;
+            d = (BigInt(D) - m*m)/d;
+            a = (BigInt(boundary) + m)/d;
+            [numerator,prevNumerator] = [a*numerator + prevNumerator,numerator];
+            [denominator,prevDenominator] = [a*denominator + prevDenominator,denominator];
+        }
+
+        if (numerator > biggestX) {
+            biggestX = numerator;
+            result = D;
+        }
+    }
+    return result;
+}
